94b4b1366a9c1548d8057a8de745db74816f5b8f
Becky Phan <beckyphan@live.com>
2019-05-24 15:50:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length > 0 
-    katz_deli.each_with_index do |name, index|
-      print "#{index + 1}. #{name}"
-    end
+    # print "The line is currently:"
+    # katz_deli.each_with_index do |name, index|
+    #   print " #{index + 1}. #{name}"
+    # end
   else 
     puts "The line is currently empty."
   end
